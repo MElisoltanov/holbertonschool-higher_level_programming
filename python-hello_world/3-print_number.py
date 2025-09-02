e8da410b458a313fb0e2c1b60d4a19deae1a5d24
MElisoltanov <elisoltanov.moussa@outlook.fr>
2025-09-02 10:48:37 +0200

    Print integer followed by Battery street using f-string

---
+++ python-hello_world/3-print_number.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# This script prints the number 98 followed by the text 'Battery street'.
+# It demonstrates how to use f-strings to format output in Python.
+
+number = 98 # Assigns the value 98 to the variable 'number'
+
+print(f"{number} Battery street") # Prints the value of 'number' and the text 'Battery street' using an f-string
