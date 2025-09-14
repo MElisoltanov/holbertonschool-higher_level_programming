f1fc76e05e8f7d90a6fd0f4d52fe7d8e50a63590
MElisoltanov <elisoltanov.moussa@outlook.fr>
2025-09-14 15:50:47 +0200

    Add say_my_name: prints formatted name with input validation

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module for say_my_name function.
+
+This module provides a function to print a formatted name.
+"""
+
+def say_my_name(first_name, last_name=""):
+    """Prints 'My name is <first name> <last name>'.
+
+    Args:
+        first_name: string, required.
+        last_name: string, optional.
+
+    Raises:
+        TypeError: If first_name or last_name is not a string.
+    """
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
