c4faf5f3b309737d2adf0f32e24f87d75655ebce
MElisoltanov <elisoltanov.moussa@outlook.fr>
2025-09-16 14:06:12 +0200

    Add 3-square.py: Square class with area method and size validation

---
+++ python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Define a class Square with a private size attribute."""
+
+
+class Square:
+    """Class that define a Square by its size."""
+    def __init__(self, size=0):
+        """Initialize a new Square.
+        Args:
+            Size (int): The size of the square.
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Return the current square area."""
+        return self.__size * self.__size
