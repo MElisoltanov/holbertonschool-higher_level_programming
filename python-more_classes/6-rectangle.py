7a132e6904a284a90913b0f1582e27afe6c46eee
MElisoltanov <elisoltanov.moussa@outlook.fr>
2025-09-19 11:21:30 +0200

    Fix: pycodestyle

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -52,7 +52,9 @@ class Rectangle:
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
-        """Return the string representation of the rectangle with '#' characters."""
+        """Return the string representation of
+        the rectangle with '#' characters.
+        """
         if self.__width == 0 or self.__height == 0:
             return ""
         return "\n".join("#" * self.__width for _ in range(self.__height))
@@ -62,6 +64,8 @@ class Rectangle:
         return f"Rectangle({self.__width}, {self.__height})"
 
     def __del__(self):
-        """Print a message when a Rectangle instance is deleted and decrement counter."""
+        """Print a message when a Rectangle instance is deleted
+        and decrement counter.
+        """
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
