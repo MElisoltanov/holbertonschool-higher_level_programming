cde6014e4c2b60b394e1eb5f21f47fa1aec5bfa0
MElisoltanov <elisoltanov.moussa@outlook.fr>
2025-09-24 10:56:40 +0200

    Fix 4-inherits_from.py: add detailed docstring and ensure correct subclass check

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+This module provides a function to determine if an object is an instance
+of a subclass of a specified class, but not an instance of the class itself.
+"""
 
 
 def inherits_from(obj, a_class):
+    """
+    Returns True if obj is an instance of
+    a subclass of a_class (not a_class itself).
+    """
     return isinstance(obj, a_class) and type(obj) is not a_class
