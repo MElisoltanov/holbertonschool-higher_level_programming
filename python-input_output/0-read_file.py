2bd05de8c2aa0bd63359dd8a8b6b07352eefb0a7
MElisoltanov <elisoltanov.moussa@outlook.fr>
2025-10-01 21:45:05 +0200

    Add 0-read_file.py: function to read and print a text file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+# filepath: python-input_output/0-read_file.py
 """Reads a text file (UTF8) and prints it to stdout."""
 
 
 def read_file(filename=""):
-    """Reads a texte file and prints its content to stdout."""
+    """Reads a text file and prints its content to stdout."""
     with open(filename, encoding="utf-8") as f:
-        print(f.read(, end=""))
+        print(f.read(), end="")
