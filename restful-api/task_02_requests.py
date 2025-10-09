313d5597155180125e1d338ff7b2a24c8ecf8f2a
MElisoltanov <elisoltanov.moussa@outlook.fr>
2025-10-09 14:52:54 +0200

    Add task_02_requests.py: fetch and print posts, fetch and save posts to CSV from API

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import requests
+import csv
+
+url = "https://jsonplaceholder.typicode.com/posts"
+
+def fetch_and_print_posts():
+    response = requests.get(url)
+    print(f"Status Code: {response.status_code}")
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print(post['title'])
+
+def fetch_and_save_posts():
+    response = requests.get(url)
+    if response.status_code == 200:
+        posts = response.json()
+        data = [{'id': post['id'], 'title': post['title'], 'body': post['body']} for post in posts]
+        with open('posts.cv', 'w', newline='', encoding='utf-8') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=['id', 'title', 'body'])
+            writer.writeheader()
+            writer.writerows(data)
